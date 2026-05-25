f8dd41b3370cdb7d663ccaa19dd7028acf43b053
Frissonx <frisson.x96@gmail.com>
2026-05-25 14:33:50 +0300

    Day 3: refactor loops, and primes challenge

---
+++ day-03-loops/index.js
@@ -0,0 +1,95 @@
+// Задание 1.
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+}
+
+for (let j = 50; j >= 30; j--) {
+    console.log(j);
+}
+
+for(let k = 0; k <= 30; k += 2) {
+    console.log(k);
+}
+
+// Задание 2.
+let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+    sum += i;
+}
+console.log(sum);
+
+// Задание 3.
+const prices = [100, 250, 75, 480, 999, 50];
+let summa = 0;
+let max = prices[0];
+
+for (let i = 0; i < prices.length; i++) {
+    console.log(`Товар #${i + 1}: ${prices[i]} руб`);
+}
+
+for (const price of prices) {
+    summa += price;
+}
+console.log(summa);
+
+for (const price of prices) {
+    if (price > max) {
+        max = price;
+    }
+}
+console.log(max);
+
+// Задание 4.
+const number = 7;
+
+for (let i = 1; i <= 10; i++) {
+    let multi = number * i;
+    console.log(`${number} * ${i} = ${multi} `);
+}
+console.log('---')
+
+for (let i = 2; i <= 9; i++) {
+    for (let j = 1; j <= 10; j++) {
+        let multi = i * j;
+        console.log(`${i} * ${j} = ${multi}`)
+    }
+    console.log('---');
+}
+// Задание 5.
+const users = ['Anna', 'Boris', 'Carol', 'David', 'Elena', 'Frank'];
+const search = 'David';
+let found = false;
+for (let i = 0; i < users.length; i++) {
+    if(users[i] === search) {
+        console.log(`Found at index ${i}`)
+        found = true;
+        break;
+    }
+}
+if (!found) {
+    console.log('Not found.');
+}
+
+// Задание 6.
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) continue;
+    console.log(i);
+}
+// Задание 7.(опцианально)
+
+for (let n = 2; n <= 50; n++) {
+    let isPrime = true;
+
+    for (let div = 2; div < n; div ++) {
+        if (n % div === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        console.log(n);
+    }
+}
+
+
