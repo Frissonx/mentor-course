bb633c8fae2d5dcb8154cb892bde6a8cdff8631e
Frissonx <frisson.x96@gmail.com>
2026-06-18 17:56:32 +0300

    Day 16: list rendering with map and keys.

--- day-16-react/src/App.jsx
+++ day-16-react/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+function ProductCard({ title, price }) {
   return (
     <div>
-      <p>Нажатий: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Прибавь</button>
-      <button onClick={() => setCount(count - 1)}>Вычти</button>
-      <button onClick={() => setCount(0)}>Сброс</button>
+      <h1>{title}</h1>
+      <p>Цена: {price} рублей</p>
+    </div>
+  );
+}
+
+function App() {
+  const products = [
+    { id: 1, title: "Burn", price: 120 },
+    { id: 2, title: "RedBull", price: 230 },
+    { id: 3, title: "Flash", price: 100 },
+  ];
+  return (
+    <div>
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          title={product.title}
+          price={product.price}
+        />
+      ))}
     </div>
   );
 }
