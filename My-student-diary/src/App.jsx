01c628eb7f214e7f77bf351c3b0bc5c01236c924
Frissonx <frisson.x96@gmail.com>
2026-07-07 07:29:48 +0300

    Continue job

--- My-student-diary/src/App.jsx
+++ My-student-diary/src/App.jsx
@@ -4,11 +4,10 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [input, setInput] = useState("");
   const [search, setSearch] = useState("");
-  
 
-  function handleAddTask() {
+  function handleAddTask(e) {
     const newTask = { id: Date.now(), text: input, completed: false };
-
+    e.preventDefault();
     if (input.trim() !== "") {
       setTasks([...tasks, newTask]);
       setInput("");
@@ -17,8 +16,26 @@ function App() {
 
   return (
     <div>
-      <input value={input} placeholder="Задача" onChange={(e) => setInput(e.target.value)} z />
-      <button onClick={handleAddTask}>Добавить</button>
+      <h1>Мои задачи</h1>
+      <form onSubmit={handleAddTask}>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Что сделать?"
+        />
+        <button type="submit">Добавить</button>
+      </form>
+      <p>Задач: {tasks.length}</p>
+      <ul>
+        {tasks.map((task, id) => (
+          <li key={task.id}>
+            {task.text}
+            <button onClick={() => setTasks(tasks.filter((_, i) => i !== id))}>
+              x
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
